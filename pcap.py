3d9ecfbf96a023b63e67e7d16abd069ee4f91de0
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2022-03-29 15:02:35 +0000

    New Scenerio

--- pcap.py
+++ pcap.py
@@ -21,30 +21,17 @@ class cap:
                     if hasattr(pak.http2, "headers_status"):
                         method = pak.http2.headers_status
 
-                    if "GET" in pak.http2.header:
-                        if "nf-instances" in str(pak.http2):
-                            method = "GET"
-
-                    if "PATCH" in pak.http2.header:
-                        #id = str(pak.http2).split("nf-instances/")[1].split("\n")[0]
-                        method = "PATCH"
-
-                    if "PUT" in pak.http2.header:
-                        id = str(pak.http2).split("nf-instances/")[1].split("\n")[0]
-                        method = "PUT"
-
-                    if "POST" in pak.http2.header:
-                        method = "POST"
-
-                    if "DELETE" in pak.http2.header:
-                        method = "DELETE"
+                    if hasattr(pak.http2, "headers_method"):
+                        method = pak.http2.headers_method
 
                     if method != "":
-                        data.append([pak.frame_info.number,
-                                pak.ip.src,
-                                pak.ip.dst,
-                                method,
-                                pak.http2.header])
+                        data.append([
+                            pak.frame_info.number,
+                            pak.ip.src,
+                            pak.ip.dst,
+                            method,
+                            pak.http2.header
+                        ])
         return data        
 
 if __name__ == "__main__":
@@ -52,7 +39,7 @@ if __name__ == "__main__":
     if len(sys.argv) >1:
         jsonConfig = json.load(open(sys.argv[1])) 
     else:
-        jsonConfig = json.load(open("scenarios/data.json"))
+        jsonConfig = json.load(open("scenarios/pdu_session_kurulumu.json"))
 
     pcapFileName = jsonConfig["pcap"]
     filters = jsonConfig["filters"]
